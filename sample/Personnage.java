81615a8b8598b3d72feaf0d22e272f023745381f
Mercuretony <mercuretony@gmail.com>
2019-04-12 17:07:57 -0400

    Création de la classe abstract pour les obstacles

--- sample/Personnage.java
+++ sample/Personnage.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Personnage {
+public abstract class Personnage {
+    private int xCoordinate;
+    private int yCoordinate;
+    private int radius;
+
+    public Personnage(int xCoordinate,int yCoordinate,int radius){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public void setyCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
 }
