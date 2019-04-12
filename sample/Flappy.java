8bf9d460c24ade4517ff5c2cca0eb38f633784bc
Mercuretony <mercuretony@gmail.com>
2019-04-12 17:19:37 -0400

    Flappy ghost is alive

---
+++ sample/Flappy.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Flappy extends Personnage {
+    int x_Coordinate;
+    int y_Coordinate;
+    public static final int radius = 30;
+
+    public Flappy(int x, int y){
+        super(x,y,Flappy.radius);
+    }
+}
